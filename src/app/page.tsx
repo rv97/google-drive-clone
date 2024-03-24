e32f512d0cc1949da9558a7809e8a743a9fae5d9
Vignesh R <vignesh.r02@sap.com>
2024-03-24 16:41:39 +0530

    Authorization based on OrgID and tokenIdentifier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignOutButton, SignedIn, SignedOut, useOrganization, useSession } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, SignedIn, SignedOut, useOrganization, useSession, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import Image from "next/image";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-  const { organization } = useOrganization();
-  const orgId = organization?.id;
+  const organization = useOrganization();
+  const user = useUser();
+
+  let orgId: string | undefined = undefined;
+  if (organization.isLoaded && user.isLoaded) {
+    orgId = organization.organization?.id ?? user.user?.id;
+  }
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
   const createFile = useMutation(api.files.createFile)
   return (
@@ -27,10 +32,10 @@ export default function Home() {
         return <div key={file._id}>{file.name}</div>
       })}
       <Button onClick={() => {
-        if (!organization) return;
+        if (!orgId) return;
         createFile({
           name: "Hello World",
-          orgId: organization.id
+          orgId
         })
       }}> Click me</Button>
     </main>
